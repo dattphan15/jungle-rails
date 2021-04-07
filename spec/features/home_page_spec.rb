c9b5380469dcff229260237eab5809b7ba871b5a
Kevin Phan <ken.phan@live.ca>
2021-04-07 00:02:38 +0000

    rspec test see product details after click

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -17,7 +17,7 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "They see all products on home page" do
     # ACT
     visit root_path
 
---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page and clicks on product to see product details", type: :feature, js: true do
+  
+    # SETUP
+    before :each do
+      @category = Category.create! name: 'Apparel'
+  
+      10.times do |n|
+        @category.products.create!(
+          name: Faker::Hipster.sentence(3),
+          description: Faker::Hipster.paragraph(4),
+          image: open_asset('apparel1.jpg'),
+          quantity: 10,
+          price: 64.99
+        )
+      end
+    end
+
+
+    scenario "They see all products on home page" do
+      # ACT
+      visit root_path
+      
+      # DEBUG / VERIFY
+      save_screenshot
+  
+      # since each _product partial renders an article with class product, we are expecting to find at least one on the page.
+      expect(page).to have_css 'article.product', count: 10
+    end
+
+
+    scenario "They see see product details after clicking on product from home page" do
+      # ACT
+      visit root_path
+      find(:xpath, '/html/body/main/section/div/article[1]/header/a/h4').click
+      
+      # DEBUG / VERIFY
+      save_screenshot
+  
+      # since each _product partial renders an article with class product, we are expecting to find at least one on the page.
+      expect(page).to have_css 'article.product-detail'
+    end
+
+
+end
